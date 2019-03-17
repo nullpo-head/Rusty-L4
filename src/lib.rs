dffe462f86cff867f43efb4a847d247cb45a2d59
Takaya Saeki <abc.tkys+pub@gmail.com>
2019-03-17 23:44:23 +0900

    Multiboot2 + Rust kernel works

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
 #![no_std]
-#![no_main]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello, World!";
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn rust_start() -> ! {
     /*let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
